22416c174a14ce382273bb4206c29d6d6970e144
Grey P <greytpearson@gmail.com>
2023-01-20 14:10:23 -0700

    started schema and seeds file

---
+++ develop/db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (department_name)
+VALUES  ("HR"),
+        ("IT"),
+        ("production");
+
+INSERT INTO role (title, salery, department_id)
+VALUES  ("engineer", 75, 3),
+        ("manager", 150, 1),
+        ("accountant", 100, 1),
+        ("janitor", 999, 2),
+        ("coordinator", 1, 3);
+       
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("jean", "pants", 4, 3),
+        ("cam", "shirt", 4),
+        ("bob", "socks", 2),
+        ("robert", "hat", 1, 2),
+        ("shaq", "basketbal", 3),
+        ("velma", "dog", 1, 1);
