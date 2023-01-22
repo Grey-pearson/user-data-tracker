08dd8bf27018b4380649e291e764f6e2d81ce2a3
Grey P <greytpearson@gmail.com>
2023-01-22 11:32:32 -0700

    working on schema

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -4,9 +4,8 @@ CREATE DATABASE user_db;
 USE user_db;
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30),
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
 );
 
 CREATE TABLE position(
@@ -16,19 +15,21 @@ CREATE TABLE position(
     department_id INT,
     -- FOREIGN KEY(department_id) REFERENCES department(id)
     -- ON DELETE SET NULL
-    ADD CONSTRAINT sr_fk_emp_man 
-    FOREIGN KEY (department_id)
+    FOREIGN KEY (department_id) 
     REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT NULL,
+    manager_id INT,
     FOREIGN KEY (role_id) REFERENCES position(id)
     -- figure out the manager id smthing idk
     -- manager_id INT NULL,
-    CONSTRAINT sr_fk_emp_man FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
